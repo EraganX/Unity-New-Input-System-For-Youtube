afd8defc71771ae334cc1300e50afa2ff14467aa
Charith Eranga <91379491+EraganX@users.noreply.github.com>
2025-10-28 21:29:20 +0530

    Game Input Manager

--- Assets/02_Scripts/Movement/PlayerMove_Translate.cs
+++ Assets/02_Scripts/Movement/PlayerMove_Translate.cs
@@ -5,48 +5,44 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMove_Translate : MonoBehaviour
 {
+    [Header("Move")]
     public float speed = 5f;
-
     private Vector2 moveInput;
     private Vector3 move;
-    private PlayerInputActions inputActions;
 
     [Header("Jump")]
     private Rigidbody rb;
     private float jumpForce = 5f;
     public ForceMode force;
+    private bool isJumping;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        inputActions = new PlayerInputActions(); //Assign Input Action
-    }
-
-    private void OnEnable()
-    {
-        inputActions.Enable(); //Input Action Enable
-
-        inputActions.Player.Jump.performed += OnJumpPerform; //Subcribe Jump Action
-    }
-
-
-
-    private void OnDisable()
-    {
-        inputActions.Player.Jump.performed -= OnJumpPerform; //Unsubcribe Jump Action
-        inputActions.Disable(); //Input Action Disble
     }
 
     private void Update()
     {
-        moveInput = inputActions.Player.Move.ReadValue<Vector2>();  //Get Keyboad Input: Move
+        moveInput = GameInputManager.Instance.GetMoveInput();
         move = new Vector3(moveInput.x, 0, moveInput.y);            //Set Move Direction
 
         transform.Translate(move * speed * Time.deltaTime, Space.World); //Object Move Using Transform.Translate
+
+        if (isJumping == false && GameInputManager.Instance.GetJumpInput())
+        {
+            isJumping = true;
+        }
     }
 
-    private void OnJumpPerform(InputAction.CallbackContext context)
+    private void FixedUpdate()
     {
-        rb.AddForce(Vector3.up * jumpForce, force);
+        if (isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce, force);
+            isJumping = false;
+        }
     }
+
+
 }
